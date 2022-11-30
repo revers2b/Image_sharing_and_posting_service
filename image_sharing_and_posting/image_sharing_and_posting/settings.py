58158a0dfd404d832e6872ce55012d06bfff26f5
Inaros <vkevinas99@gmail.com>
2022-11-30 13:17:31 +0200

    -added to "INSTALLED_APPS"

--- image_sharing_and_posting/image_sharing_and_posting/settings.py
+++ image_sharing_and_posting/image_sharing_and_posting/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'img_share_post',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
