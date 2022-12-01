de0849351aa83549fec9fe5562fd592b899212b4
Inaros <vkevinas99@gmail.com>
2022-12-01 09:21:30 +0200

    -Uploads/creating posts

--- image_sharing_and_posting/img_share_post/models.py
+++ image_sharing_and_posting/img_share_post/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Upload(models.Model):
+    title = models.CharField(("Title"), max_length=100)
+    description = models.TextField(("Description"), max_length=20000)
+    user = models.ForeignKey(
+        User,
+        verbose_name=("user"),
+        on_delete=models.CASCADE,
+        related_name="posts",
+    )
+    image = models.ImageField(("image"), upload_to="user_images/", blank=True, null=True)
+
+    def __str__(self) -> str:
+        return ("{title} by {user}").format(title=self.title, user=self.user)
