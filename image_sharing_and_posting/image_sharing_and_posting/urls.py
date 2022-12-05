8e1e806fc8c77146122dcf56dcf75d5f31edc63c
Inaros <vkevinas99@gmail.com>
2022-12-05 08:42:14 +0200

    -homepage

--- image_sharing_and_posting/image_sharing_and_posting/urls.py
+++ image_sharing_and_posting/image_sharing_and_posting/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('home', include('img_share_post.urls')),
+    path('', include('img_share_post.urls')),
     path('admin/', admin.site.urls),
 ]
--- image_sharing_and_posting/img_share_post/views.py
+++ image_sharing_and_posting/img_share_post/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Test")
+    template = "image_sharing_and_posting/home.html"
+    return render(request, template)
