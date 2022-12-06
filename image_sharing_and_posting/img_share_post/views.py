4ca93afa1c5d4af15e3608875ad5ea23a2c5dae0
Inaros <vkevinas99@gmail.com>
2022-12-06 13:07:37 +0200

    home html for front page, base css for page style,

--- image_sharing_and_posting/img_share_post/views.py
+++ image_sharing_and_posting/img_share_post/views.py
@@ -3,5 +3,4 @@ from django.http import HttpResponse
 
 def home(request):
     template = "image_sharing_and_posting/home.html"
-    context = {}
-    return render(request, template, context)
+    return render(request, template)
