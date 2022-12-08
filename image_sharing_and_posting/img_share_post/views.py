bb7d2c23c9563d9e9c45598c563ec4b967c69083
Inaros <vkevinas99@gmail.com>
2022-12-08 22:19:14 +0200

    profile url, homepage visual changes

--- image_sharing_and_posting/img_share_post/views.py
+++ image_sharing_and_posting/img_share_post/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
 from django.http import HttpResponse
+
+User = get_user_model
 
 def home(request):
     template = "image_sharing_and_posting/home.html"
+    return render(request, template)
+
+def profile(request):
+    template = "image_sharing_and_posting/Profile.html"
     return render(request, template)
