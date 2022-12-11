ab867420989b78dc14009112223219d746aa8021
Inaros <vkevinas99@gmail.com>
2022-12-11 23:40:15 +0200

    img, articles/posts

--- image_sharing_and_posting/image_sharing_and_posting/settings.py
+++ image_sharing_and_posting/image_sharing_and_posting/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from . import local_settings
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,7 +124,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR.joinpath('static/')
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR.joinpath('media')
+MEDIA_ROOT = os.path.join('media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- image_sharing_and_posting/img_share_post/models.py
+++ image_sharing_and_posting/img_share_post/models.py
@@ -1,58 +1,96 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from tinymce.models import HTMLField
+from django.template.defaultfilters import slugify
+from django.utils import timezone
+import os
 
 User = get_user_model()
 
 
-class Upload(models.Model):
-    title = models.CharField(("Title"), max_length=100)
-    description = HTMLField("Description")
-    user = models.ForeignKey(
-        User,
-        verbose_name=("user"),
-        on_delete=models.CASCADE,
-        related_name="posts",
-    )
-    image = models.ImageField(("image"), upload_to="images/", blank=True, null=True)
+class ArticleSeries(models.Model):
+    def image_upload_to(self, instance=None):
+        if instance:
+            return os.path.join("ArticleSeries", slugify(self.slug), instance)
+        return None
 
-    def __str__(self) -> str:
-        return ("{title} by {user}").format(title=self.title, user=self.user)
+    title = models.CharField(max_length=200)
+    subtitle = models.CharField(max_length=200, default="", blank=True) 
+    slug = models.SlugField("Series slug", null=False, blank=False, unique=True)
+    published = models.DateTimeField("Date published", default=timezone.now)
+    author = models.ForeignKey(get_user_model(), default=1, on_delete=models.SET_DEFAULT)
+    image = models.ImageField(default='default/no_image.jpg', upload_to=image_upload_to ,max_length=255)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "Series"
+        ordering = ['-published']
+
+class Article(models.Model):
+    def image_upload_to(self, instance=None):
+        if instance:
+            return os.path.join("ArticleSeries", slugify(self.series.slug), slugify(self.article_slug), instance)
+        return None
+
+    title = models.CharField(max_length=200)
+    subtitle = models.CharField(max_length=200, default="", blank=True)
+    article_slug = models.SlugField("Article slug", null=False, blank=False, unique=True)
+    content = HTMLField(blank=True, default="")
+    notes = HTMLField(blank=True, default="")
+    published = models.DateTimeField("Date published", default=timezone.now)
+    modified = models.DateTimeField("Date modified", default=timezone.now)
+    series = models.ForeignKey(ArticleSeries, default="", verbose_name="Series", on_delete=models.SET_DEFAULT)
+    author = models.ForeignKey(get_user_model(), default=1, on_delete=models.SET_DEFAULT)
+    image = models.ImageField(default='default/no_image.jpg', upload_to=image_upload_to ,max_length=255)
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def slug(self):
+        return self.series.slug + "/" + self.article_slug
+
+    class Meta:
+        verbose_name_plural = "Article"
+        ordering = ['-published']
 
 
-class Like(models.Model):
-    upload = models.ForeignKey(
-        Upload,
-        verbose_name=("upload"),
-        on_delete=models.CASCADE,
-        related_name="Like"
-    )
-    user = models.ForeignKey(
-        User, verbose_name=("User"),
-        on_delete=models.CASCADE,
-        related_name="Upload_like",
-    )
+# class Like(models.Model):
+#     upload = models.ForeignKey(
+#         ArticleSeries,
+#         verbose_name=("upload"),
+#         on_delete=models.CASCADE,
+#         related_name="Like"
+#     )
+#     user = models.ForeignKey(
+#         User, verbose_name=("User"),
+#         on_delete=models.CASCADE,
+#         related_name="Upload_like",
+#     )
 
-    def __str__(self) -> str:
-        return f"{self.user} likes {self.upload}"
+#     def __str__(self) -> str:
+#         return f"{self.user} likes {self.upload}"
 
-class comment(models.Model):
-    upload = models.ForeignKey(
-        Upload,
-        verbose_name=("upload"),
-        on_delete=models.CASCADE,
-        related_name="Comments",
-    )
-    body = models.TextField(("comment"), max_length=200)
-    user = models.ForeignKey(
-        User,
-        verbose_name=("User"),
-        on_delete=models.CASCADE,
-        related_name="comments"
-    )
 
-    def __str__(self) -> str:
-        return ("Commented on {upload_id}, by {user}").format(
-            upload_id=self.upload.id,
-            user=self.user,
-        )
+# class comment(models.Model):
+#     upload = models.ForeignKey(
+#         ArticleSeries,
+#         verbose_name=("upload"),
+#         on_delete=models.CASCADE,
+#         related_name="Comments",
+#     )
+#     body = models.TextField(("comment"), max_length=200)
+#     user = models.ForeignKey(
+#         User,
+#         verbose_name=("User"),
+#         on_delete=models.CASCADE,
+#         related_name="comments"
+#     )
+
+#     def __str__(self) -> str:
+#         return ("Commented on {upload_id}, by {user}").format(
+#             upload_id=self.upload.id,
+#             user=self.user,
+#         )
